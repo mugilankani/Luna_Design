ef7f147060142314e6ec1e9b5fb49b0de885462c
Mugilan Sakthivel <mugilankani951@gmail.com>
2025-11-09 16:31:24 +0530

    05: ai-jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@
 
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
+import { useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
+  const [value, setValue] = useState("");
   const trpc  = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
     onSuccess: () => {
@@ -13,7 +15,8 @@ const Page = () => {
   }));
   return ( 
     <div>
-      <button disabled={invoke.isPending} className="bg-black text-white px-4 py-2 rounded" onClick={() => invoke.mutate({ text: "Inngest from tRPC!" })}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} type="text" />
+      <button disabled={invoke.isPending} className="bg-black text-white px-4 py-2 rounded" onClick={() => invoke.mutate({ value: value })}>
         Invoke Inngest
       </button>
     </div>
