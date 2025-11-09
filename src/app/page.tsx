672c1421637988884dcac0f9635de6ff24811808
Mugilan Sakthivel <mugilankani951@gmail.com>
2025-11-09 09:55:20 +0530

    02:database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button";
 
 const Page = () => {
+
   return ( 
     <div>
-      <h1 className="font-bold">Welcome to the Home Page</h1>
-      <p>This is the main content of the page.</p>
-      <Button>Click Me</Button>
+      hello world 
     </div>
    );
 }
